b7539ca2f67d83c72a9c2cbc26a1d5e2c8d01318
ColeDewis <coledewis@gmail.com>
2025-11-25 13:31:40 -0700

    update record tmux script

--- startup/record_start.py
+++ startup/record_start.py
@@ -4,6 +4,17 @@ from os import path
 
 import libtmux
 
+
+def get_next_output_dir(base_folder, prefix="dataset-"):
+    existing_dirs = [
+        d
+        for d in os.listdir(base_folder)
+        if path.isdir(path.join(base_folder, d)) and d.startswith(prefix) and d[len(prefix):].isdigit()
+    ]
+    existing_indices = [int(d[len(prefix):]) for d in existing_dirs]
+    next_index = max(existing_indices) + 1 if existing_indices else 1
+    return path.join(base_folder, f"{prefix}{next_index}")
+
 if __name__ == "__main__":
     server = libtmux.Server(
         config_file=path.expandvars("/home/user/kinova_flow/startup/.tmux.conf")
@@ -17,15 +28,16 @@ if __name__ == "__main__":
 
     # terminals for the simulation to start
 
-    # TODO dynamic dataset naming
+    dataset_folder = "/home/user/kinova_flow/data/"
+    dataset_name = get_next_output_dir(dataset_folder)
+
     terminals = {
         "rqt": "rqt --perspective-file /home/user/kinova_flow/startup/rqt.perspective",
         "kortex_bringup": "roslaunch kortex_bringup kortex_bringup.launch",
         "cameras": "roslaunch --wait cameras single_rs.launch",
         "joynode": 'rosparam set joy_node/dev "/dev/input/js0"\nrosrun joy joy_node',
         "kortex_joy": "rosrun joystick_control kinova_joy.py _controller:=cartesian",
-        "recorder": "rosrun recorder record.py test_dataset",
-        # "recorder": f"echo 'rosrun recorder recorder.py _output_dir:={output_dir}'",
+        "recorder": f"rosrun recorder record.py {dataset_name}",
     }
 
     for name, cmd in terminals.items():
