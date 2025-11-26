e41f945ae4cb1b87a59da705a6f681b8d0a3a476
ColeDewis <coledewis@gmail.com>
2025-11-26 14:44:40 -0700

    inference works somehow

--- catkin_ws/src/flow_inference/scripts/kinova_inference.py
+++ catkin_ws/src/flow_inference/scripts/kinova_inference.py
@@ -14,7 +14,7 @@ from sensor_msgs.msg import Image, JointState, PointCloud2
 
 class FlowInference:
     def __init__(self):
-        # self.robot = KinovaGen3()
+        self.robot = KinovaGen3()
 
         rospy.on_shutdown(self.stop_robot)
 
@@ -44,15 +44,15 @@ class FlowInference:
         ts.registerCallback(self.infer_callback)
         # for testing, create dummy input
 
-        dummy_state = np.array([np.random.rand(self.obs_window_size, 8).astype(np.float32)])
-        dummy_pc = np.array([np.random.rand(2, 1024, 6).astype(np.float32)])
+        # dummy_state = np.array([np.random.rand(self.obs_window_size, 8).astype(np.float32)])
+        # dummy_pc = np.array([np.random.rand(2, 1024, 6).astype(np.float32)])
 
-        dummy_data = {
-            "agent_pos": torch.from_numpy(dummy_state).cuda(),
-            "point_cloud": torch.from_numpy(dummy_pc).cuda()
-        }
+        # dummy_data = {
+        #     "agent_pos": torch.from_numpy(dummy_state).cuda(),
+        #     "point_cloud": torch.from_numpy(dummy_pc).cuda()
+        # }
 
-        self.model_inference(dummy_data)
+        # self.model_inference(dummy_data)
         
         self.control_loop()
 
@@ -77,10 +77,10 @@ class FlowInference:
         elif len(self.states) < self.obs_window_size:
             return
         
-        if len(self.action_buffer) > 0:
+        # empirically 2 seems to be needed in order for it to not lag behind
+        if len(self.action_buffer) > 2:
             # don't do inference if we have actions to send still
-            # NOTE this probably will induce a slight delay and in future we probably
-            # want to do inference slightly before the buffer becomes empty.
+            # NOTE 2 is kinda heuristic, but seems roughly correct
             return
         
         state_input = np.array(self.states)
@@ -97,6 +97,7 @@ class FlowInference:
         self.model_inference(data)
         
     def stop_robot(self):
+        rospy.loginfo("SHUTTING DOWN: Stopping robot.")
         self.robot.send_twist_topic(np.zeros(6))
 
     def model_inference(self, data):
@@ -105,16 +106,18 @@ class FlowInference:
             action = action_seq['action']
             actions = [a.cpu().numpy() for a in action.squeeze(0)]
             rounded_action = torch.round(action * 100) / 100
-            rospy.loginfo(f"Predicted action sequence: {rounded_action}")
+            # rospy.loginfo(f"Predicted action sequence: {rounded_action}")
 
+        # clear buffer so we never execute an old action sequence
+        self.action_buffer = []
         self.action_buffer.extend(actions)
     
     def send_safe_action(self, action):
         # TODO: also set certain vels to 0 if close to boundaries.
         action[:3] = np.clip(action[:3], -0.1, 0.1)  # xyz limits
-        action[3:6] = np.clip(action[3:6], -5, 5) # rpy limits
+        action[3:6] = np.deg2rad(np.clip(action[3:6], -5, 5))
         rospy.loginfo(f"Sending action: {action}")
-        pass
+        self.robot.send_twist_topic(action)
 
     def control_loop(self):
         rate = rospy.Rate(15)  # 15 Hz, we SHOULD have recorded demos at that frequency
@@ -123,6 +126,7 @@ class FlowInference:
         while not rospy.is_shutdown():
             if len(self.action_buffer) == 0:
                 rospy.loginfo("Waiting for next inference...")
+                self.robot.send_twist_topic(np.zeros(6))
                 rate.sleep()
                 continue
 
